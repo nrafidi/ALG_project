6140e795075f1a0b926fc6f48ff4d163f80c4017
nrafidi <nicole.rafidi@gmail.com>
2013-04-08 14:37:03 -0400

    Data processing functions (complete, I think)

--- Source/data_proc.c
+++ Source/data_proc.c
@@ -3,33 +3,79 @@
 #include <string.h>
 
 
-double * readX(void)
+int readX(double *x, char *fname)
 {
-  float n;
-  double *x;
+  double n;
+  int i,j;
   char read = 'r';  
-  int i = 0;
   const char* mode = &read;
 
-  FILE * fid = fopen("testX.txt", mode);
+  FILE * fid = fopen(fname, mode);
   
-  /* if (fid != NULL)
-    printf("sup kirstin\n");
-  else
-  perror("fopen");*/
-
-  while (fscanf(fid, "%f%*c", &n) > 0)
+  i = 0;
+  while (fscanf(fid, "%lf%*c", &n) > 0)
     {     
-      //testing
-      printf("%f\n", n);
+      //testing fscanf
+      //printf("%f\n", n);
       x[i] = n;
       i++;
-    }
-  return x;
+    } 
+  
+  /*  printf("Reprint x before returning:\n");
+  for (j = 0; j < i; j++)
+  printf("x at %d: %f\n", j, x[j]);*/
+
+  return 0;
+}
+
+int readY(int *y, char *fname)
+{
+  int n, i, j;
+  char read = 'r';  
+  const char* mode = &read;
+
+  FILE * fid = fopen(fname, mode);
+
+  i = 0;
+  while (fscanf(fid, "%d%*c", &n) > 0)
+    {     
+      y[i] = n;
+      i++;
+    } 
+
+  return 0;
 }
 
 int main(void)
 {
-  //string fname = "testX.txt";
-  double *x = readX();
+  int j, k;
+  int num_feats = 2;
+  int num_samp = 3;
+  char *fname = "testX.txt";
+  char *fYname = "testY.txt";
+
+  //Client must malloc and free these
+  double *x = malloc(num_feats*num_samp*sizeof(double));
+  int *y = malloc(num_samp*sizeof(int));
+
+  //Test readX
+  readX(x, fname);
+  printf("Reprinting what was read by readX:\n");
+  for (j = 0; j < num_feats; j++)
+    {
+      for (k = 0; k < num_samp; k++)
+	{
+	  printf("%f, ", x[num_samp*j + k]);
+	}
+      printf("\n");
+    }
+
+  //Test readY
+  readY(y, fYname);
+  printf("Reprinting what was read by readY:\n");
+  for (k = 0; k < num_samp; k++)
+    printf("%d, ", y[k]);
+  printf("\n");
+  free(x);
+  free(y);
 }
